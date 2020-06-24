9c66a0d9746a6b12d559675d13e7f93452a415d1
debjyotiC <djbabai.debjyoti@gmail.com>
2020-06-24 19:39:18 +0530

    week 1 code exam.

---
+++ unit-1.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import numpy as np
+
+xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], dtype=float)
+ys = np.array([1.0, 1.5, 2.0, 2.5, 3.0, 3.5], dtype=float)
+
+l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
+model = tf.keras.Sequential([l0])
+model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
+
+model.fit(xs, ys, epochs=460)
+print("Weights are:{}".format(l0.get_weights()))
+print(model.predict([7.0])[0])
