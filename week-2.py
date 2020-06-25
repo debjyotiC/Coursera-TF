8c78ec09ccdd881dffbd9a4b98e29539bfc1066b
debjyotiC <djbabai.debjyoti@gmail.com>
2020-06-25 18:47:13 +0530

    week 2 code exam

--- week-2.py
+++ week-2.py
@@ -17,9 +17,10 @@ callbacks = myCallback()
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+    tf.keras.layers.Dense(1024, activation=tf.nn.relu),
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
+
 model.compile(optimizer=tf.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
