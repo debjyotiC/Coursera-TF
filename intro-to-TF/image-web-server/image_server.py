029683e3e1324f0cafaa77c1403a37f5c616c562
debjyotiC <djbabai.debjyoti@gmail.com>
2020-07-12 12:35:43 +0530

    added classification saver csv

--- intro-to-TF/image-web-server/image_server.py
+++ intro-to-TF/image-web-server/image_server.py
@@ -7,6 +7,7 @@ import datetime
 from flask import Flask, render_template, request, session
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 from keras.preprocessing import image
 
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
@@ -24,6 +25,12 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def write_to_csv(num, clf):
+    values = {'Image': [num], 'classified': [clf]}
+    df_w = pd.DataFrame(values, columns=['Image', 'classified'])
+    df_w.to_csv("/home/pi/Coursera-TF/intro-to-TF/image-web-server/results.csv", index=None, header=True)
 
 
 @app.route('/')
@@ -53,11 +60,13 @@ def image_classify():
     if request.method == 'POST':
         from_path = session['path']
         # load_model = tf.keras.models.load_model('saved_model/my_model')
-        load_model = tf.lite.Interpreter(model_path="/home/pi/Coursera-TF/intro-to-TF/image-web-server/saved_model/tflite_model/converted_model.tflite")
+        load_model = tf.lite.Interpreter(
+            model_path="/home/pi/Coursera-TF/intro-to-TF/image-web-server/saved_model/tflite_model/converted_model.tflite")
         load_model.allocate_tensors()
         input_details = load_model.get_input_details()
         output_details = load_model.get_output_details()
-        img = image.load_img('/home/pi/Coursera-TF/intro-to-TF/image-web-server/static/uploaded_images/' + from_path, target_size=(300, 300))
+        img = image.load_img('/home/pi/Coursera-TF/intro-to-TF/image-web-server/static/uploaded_images/' + from_path,
+                             target_size=(300, 300))
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         images = np.vstack([x])
@@ -66,9 +75,13 @@ def image_classify():
         load_model.invoke()
         classes = load_model.get_tensor(output_details[0]['index'])
         if classes[0] > 0.5:
+            write_to_csv(str(from_path), 1)
+            print(str(from_path))
             return render_template('index.html', uploaded_image=from_path,
                                    classification="uploaded image is of a human")
         else:
+            write_to_csv(str(from_path), 0)
+            print(str(from_path))
             return render_template('index.html', uploaded_image=from_path,
                                    classification="uploaded image is of a horse")
 
