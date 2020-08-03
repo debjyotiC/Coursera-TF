16de859fcacbdfcde665184c26c5ee164afb8af4
debjyotiC <djbabai.debjyoti@gmail.com>
2020-08-03 12:35:49 +0530

    added code for NLP class

---
+++ nlp-with-TF/week1.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+sentences = ['I love my dog',
+             'I love my cat',
+             'You love my dog!',
+             'Do you think my dog is amazing?']
+
+tokenizer = Tokenizer(num_words=100, oov_token="<OOV>")
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(sentences)
+print(word_index)
+test_data = ['I really love my dog',
+             'My dog loves my manatee']
+
+test_seq = tokenizer.texts_to_sequences(test_data)
+print(test_seq)
