fcb34166ffbe66366cc30f69bc2a69adac634257
debjyotiC <djbabai.debjyoti@gmail.com>
2020-11-04 20:04:58 +0530

    added Cats vs Dog images

--- nlp-with-TF/week1.py
+++ nlp-with-TF/week1.py
@@ -1,19 +1,23 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
-
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 sentences = ['I love my dog',
              'I love my cat',
-             'You love my dog!',
-             'Do you think my dog is amazing?']
+             'You love my dog!']
+             # 'Do you think my dog is amazing?']
 
-tokenizer = Tokenizer(num_words=100, oov_token="<OOV>")
+tokenizer = Tokenizer(num_words=100)
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
-sequences = tokenizer.texts_to_sequences(sentences)
 print(word_index)
-test_data = ['I really love my dog',
-             'My dog loves my manatee']
-
-test_seq = tokenizer.texts_to_sequences(test_data)
-print(test_seq)
+# sequences = tokenizer.texts_to_sequences(sentences)
+#
+# test_data = ['I really love my dog',
+#              'My dog loves my manatee']
+#
+# test_seq = tokenizer.texts_to_sequences(test_data)
+# padded = pad_sequences(sequences, padding='post')
+# print(word_index)
+# print(sequences)
+# print(padded)
